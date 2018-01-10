6eac7d42a14fb68c13877e1246ba889b734244fa
hotseklots <30752732+hotseklots@users.noreply.github.com>
2018-01-10 19:33:54 +0100

    Moved project to own folder

--- generatedIP/dualPortedRam_inst.vhd
+++
@@ -1,8 +0,0 @@
-dualPortedRam_inst : dualPortedRam PORT MAP (
-		clock	 => clock_sig,
-		data	 => data_sig,
-		rdaddress	 => rdaddress_sig,
-		wraddress	 => wraddress_sig,
-		wren	 => wren_sig,
-		q	 => q_sig
-	);
